6017437b3c9c6f91b614d69e0823f2f1903af4e6
VladDraga <draga260806@gmail.com>
2025-02-17 15:59:37 +0100

    dz2

---
+++ dz.js
@@ -0,0 +1,54 @@
+// 1
+
+const celsiu = 20;
+const result = celsiu * 9 / 5 + 32;
+console.log(result);
+
+// 2
+
+const Days = 31;
+const resultOne = Days * 24; 
+console.log(resultOne);
+
+const resultTwo = resultOne * 60;
+console.log(resultTwo);
+
+// 3
+const Helth = 100;
+const Energy = 50;
+const resultHelth = Helth - 50;
+console.log(resultHelth);
+const resultEnergy = Energy - 10;
+console.log(resultEnergy);
+
+// 4
+
+const Sum = 100;
+const resultSum = Sum * 0.10;
+console.log(resultSum);
+
+// 5
+
+const numberOne = 5.5;
+console.log(Math.floor(numberOne));
+
+// 6
+
+const numberTwo = "12px";
+console.log(Number.parseFloat(numberTwo));
+
+// 7
+
+const numberThree = "18";
+console.log(Number.parseInt(numberThree));
+
+// 8
+
+const numberFour = 144;
+console.log(Math.sqrt(numberFour));
+
+// 9
+
+const numberFive = "21px";
+console.log(Number.parseInt(numberFive));
+console.log(numberFive.toString(numberFive));
