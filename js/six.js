03d1c611d803887796c2842eff8ab95a1abd312b
VladDraga <draga260806@gmail.com>
2025-03-22 22:06:01 +0100

    dz666

--- js/six.js
+++ js/six.js
@@ -1,45 +1,45 @@
 // Задача 1
 
-let i = 0;
-while (i <= 10) {
-    console.log(i);
-    i += 1
+let i1 = 0;
+while (i1 <= 10) {
+    console.log(i1);
+    i1 += 1
 }
 
 
 // Задача 2
 
-for (let i = 0; i <= 20; i += 1) {
-     if (i % 2 !== 0) {
+for (let i2 = 0; i2 <= 20; i2 += 1) {
+     if (i2 % 2 !== 0) {
          continue;
     }
-    console.log(i);
+    console.log(i2);
  }
 
 // Задача 3
 
 let num = 7;
-for (let i = 0; i <= 10; i += 1) {
-    console.log(`${num} * ${i} = ${num * i}`);
+for (let i3 = 0; i3 <= 10; i3 += 1) {
+    console.log(`${num} * ${i3} = ${num * i3}`);
 }
 
 // Задача 4
 
 const number = Number(prompt("Введіть любе число до 10"));
-for (let i = 0; i <= 10 ; i += 1){
-    if (i >= number) {
+for (let i4 = 0; i4 <= 10 ; i4 += 1){
+    if (i4 >= number) {
         break;
     }
-    console.log(i);
+    console.log(i4);
 }
 
 // Задача 5
 
-let i = 0;
-while (i <= 20) {
-    console.log(i);
-    i += 1
-      if (i % 3 === 0) {
+let i5 = 0;
+while (i5 <= 20) {
+    console.log(i5);
+    i5 += 1
+      if (i5 % 3 === 0) {
           continue;
      }
 }
